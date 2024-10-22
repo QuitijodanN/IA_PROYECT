5d2eb8ef0ca09374a30e306d7da4d848ead80fe8
SerAngGom <al405280@uji.es>
2024-10-22 09:47:12 +0200

    Audio cofre no funciona

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
     [SerializeField] private ChestManager manager;
+    
+    private AudioSource audioSource;
 
+    [SerializeField] private AudioClip chestsound;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+   
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("hola");
+        //Debug.Log("hola");
         if (collision.gameObject.CompareTag("Player"))
         {
             manager.AddCounter();
+
+            audioSource.Play();
+            
             Destroy(gameObject);
         }
 
